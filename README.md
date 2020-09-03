93a40374ff9be87cff0c4cee9df984d8be719c5a
StandaSasek <stanislav.sasek@gmail.com>
2020-09-03 13:06:52 +0200

    Re-set of content of Day folders, a new part: Basics C#

--- README.md
+++
@@ -1,2 +0,0 @@
-Please feel free to check my work in GreenFox Academy course:
-[Main work repository](../green-fox-academy/StandaSasek)
---
+++ week-01/day-4/rep/Examples/Examples/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*string name = "Standa";
+            string addition = $"Hello, my name is {name}";
+            Console.WriteLine(addition);
+
+            string input = Console.ReadLine();
+            //always check what it give you back 
+
+            bool isInputCorrect = Int32.TryParse(input, out int result);
+            //variable "bool" because TryParse returns Boolean = need to set variable to be it, but "result" will be numeric
+
+            if (isInputCorrect)
+            {
+                Console.WriteLine(result * 5)
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong input. Please use only numbers.");
+            }
+
+            int dayOfWeek = 5;
+            switch (dayOfWeek)
+            {
+                case 3:
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                default:Console.WriteLine("Ihis is not number of a week day");
+                    // "case" set to 1-7 and any other number will return "default"
+            }
+
+            int number = 5;
+            while (number < 10)
+            {
+                Console.WriteLine("Something times number count to while < 10");
+                number++; // adds 1 to "number"
+            }*/
+
+            /*            do
+                        {
+                            Console.WriteLine("Please enter a number");
+                            string input = Console.ReadLine();
+                            bool isInputCorrect = Int32.TryParse(input, out int result);
+                            if (result == true)
+                                Console.WriteLine("Success!");
+                        }
+            */
+            /*int n = 5;
+
+            if (n < 10)
+            {
+                Console.WriteLine("smaller than 10");
+            }
+
+            if (n< 8) //can be like this OR if > else if > else
+            {
+                Console.WriteLine("smaller than 8");
+
+            }
+            else
+            {
+                Console.WriteLine("whatever");
+            }*/
+
+/*            int n = 5;
+            Console.WriteLine(n++); //shows 5, add 1 was made after
+            Console.WriteLine(++n); //shows 6, add 1 was made before
+*/
+
+
+        }
+
+
+
+
+    }
+}
+}
