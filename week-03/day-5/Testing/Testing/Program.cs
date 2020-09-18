be81e365cd7b7e7da12a3b4708241ea0ae09dc6b
StandaSasek <stanislav.sasek@gmail.com>
2020-09-18 22:08:04 +0200

    Testing - not able to run test, removing of Main not helped

---
+++ week-03/day-5/Testing/Testing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Xunit;
+
+namespace Testing
+{
+    public class Program
+    {
+        static void Main()
+        {
+        }
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Addition(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Addition(2, 2));
+        }
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        [Fact]
+        public void FirstTest()
+        {
+            int expected = 0;
+            int result = Addition(0, 0);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
