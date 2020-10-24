133960eaf1823a03e5f5a978ea1c9ae847870d39
StandaSasek <stanislav.sasek@gmail.com>
2020-10-24 16:56:28 +0200

    A few katas done

--- Codewars/8kyu/Program.cs
+++ Codewars/8kyu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace _8kyu
 {
@@ -48,15 +49,8 @@ namespace _8kyu
             //("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", AlphabetPosition("The sunset sets at twelve o' clock."));
             //("20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20", AlphabetPosition("The narwhal bacons at midnight."));
 
-
             //Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock."));
             //Console.WriteLine(AlphabetPosition("The narwhal bacons at midnight."));
-
-            //Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2 }, new int[] { 1 })));
-            //Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 })));
-            //Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 })));
-            //Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { })));
-            //Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { }, new int[] { 1, 2 })));
 
             //MatrixAddition(
             //    new int[][] { new int[] { 1, 2 }, new int[] { 1, 2 } },
@@ -72,17 +66,177 @@ namespace _8kyu
             int[] numbers4 = { };
             //(13, Kata.sumTwoSmallestNumbers(numbers));
 
-            Console.WriteLine("13, " + sumTwoSmallestNumbers(numbers));
+            /*Console.WriteLine("13, " + sumTwoSmallestNumbers(numbers));
             Console.WriteLine("16, " + sumTwoSmallestNumbers(numbers1));
             Console.WriteLine("27, " + sumTwoSmallestNumbers(numbers2));
             Console.WriteLine("22, " + sumTwoSmallestNumbers(numbers3));
-            Console.WriteLine("0, " + sumTwoSmallestNumbers(numbers4));
+            Console.WriteLine("0, " + sumTwoSmallestNumbers(numbers4));*/
 
+            /*Console.WriteLine("15, " + Repeats(new List<int> { 4, 5, 7, 5, 4, 8 }));
+            Console.WriteLine("19, " + Repeats(new List<int> { 9, 10, 19, 13, 19, 13 }));
+            Console.WriteLine("12, " + Repeats(new List<int> { 16, 0, 11, 4, 8, 16, 0, 11 }));
+            Console.WriteLine("22, " + Repeats(new List<int> { 5, 17, 18, 11, 13, 18, 11, 13 }));
+            Console.WriteLine("24, " + Repeats(new List<int> { 5, 10, 19, 13, 10, 13 }));*/
+
+            /*Console.WriteLine("0, " + Solve(new int[] { }));
+            Console.WriteLine("7, " + Solve(new int[] { 1, 2, 3, 4 }));
+            Console.WriteLine("13, " + Solve(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Console.WriteLine("47, " + Solve(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }));*/
+
+            /*var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            foreach (var item in arr)
+            {
+                Console.WriteLine(IsPrime(item));
+            }*/
+
+            /* Console.WriteLine("I am not impressed by your performance." + Quote("george saint pierre"));
+             Console.WriteLine("I'd like to take this chance to apologize.. To absolutely NOBODY!" + Quote("conor mcgregor"));
+             Console.WriteLine("I am not impressed by your performance." + Quote("George Saint Pierre"));
+             Console.WriteLine("I'd like to take this chance to apologize.. To absolutely NOBODY!" + Quote("Conor McGregor"));*/
+
+            // To be a senior, a member must be at least 55 years old and have a handicap greater than 7.
+            // In this croquet club, handicaps range from -2 to + 26; the better the player the lower the handicap.
+            /*Console.WriteLine(new[] { "Open", "Senior", "Open", "Senior" } + ", " + OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+            Console.WriteLine(new[] { "Open", "Open", "Open", "Open" } + ", " + OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }));
+            Console.WriteLine(new[] { "Senior", "Open", "Open", "Open" } + ", " + OpenOrSenior(new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 } }));*/
+
+            /*Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2 }, new int[] { 1 })));
+            Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 })));
+            Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 })));
+            Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { 1, 2, 2 }, new int[] { })));
+            Console.WriteLine(String.Join(", ", ArrayDiff(new int[] { }, new int[] { 1, 2 })));*/
+
+            string s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+            Console.WriteLine("3/56 ," + PrinterError(s));
+        }
+        public static string PrinterError(String s)
+        {
+            int errors = 0;
+            foreach (var item in s)
+            {
+                if (item > 109)
+                {
+                    errors++;
+                }
+            }
+            return $"{errors}/{s.Length}";
+        }
+        public static int[] ArrayDiff(int[] a, int[] b) // TODO does not work 
+        {
+            List<int> result = new List<int>();
+            foreach (var number in a)
+            {
+                result.Add(number);
+            }
+            for (int second = 0; second < b.Length; second++)
+            {
+                for (int first = 0; first < result.Count; first++)
+                {
+                    if (result[first] == b[second])
+                    {
+                        result.RemoveAt(first);
+                        first = 0;
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            var result = new List<string>();
+            foreach (var item in data)
+            {
+                if (item[0] >= 55 && item[1] > 7)
+                {
+                    result.Add("Senior");
+                }
+                else
+                {
+                    result.Add("Open");
+                }
+            }
+            return result;
+        }
+        public static string Quote(string fighter)
+        {
+            if (fighter.ToLower() == "conor mcgregor")
+            {
+                return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+            }
+            else
+            {
+                return "I am not impressed by your performance.";
+            }
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n < 2 || n > 2 && n % 2 == 0)
+            {
+                return false;
+            }
+            if (n == 2)
+            {
+                return true;
+            }
+            for (int i = 3; i * i <= n; i = i + 2)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static int Solve(int[] arr)
+        {
+            int result = 0;
+            if (arr.Length < 3)
+            {
+                return 0;
+            }
+            List<int> primes = new List<int>() { 2 };
+            for (int i = 2; i < arr.Length; i++)
+            {
+                bool prime = true;
+                foreach (var number in primes)
+                {
+                    if (i % number == 0)
+                    {
+                        prime = false;
+                    }
+                }
+                if (prime)
+                {
+                    primes.Add(i);
+                }
+            }
+            foreach (var prime in primes)
+            {
+                result += arr[prime];
+            }
+            return result;
+        }
+        public static int Repeats(List<int> source)
+        {
+            while (source.Count > 2)
+            {
+                for (int i = 0; i < source.Count; i++)
+                {
+                    int a = source[i];
+                    for (int j = 0; j < source.Count; j++)
+                    {
+                        if (i != j && a == source[j])
+                        {
+                            source.RemoveAt(i);
+                            source.Remove(a);
+                        }
+                    }
+                }
+            }
+            return source[0] + source[1];
         }
         public static int sumTwoSmallestNumbers(int[] numbers)
         {
             if (numbers.Length < 2)
-            { 
+            {
                 if (numbers.Length == 0)
                 {
                     return 0;
@@ -120,25 +274,6 @@ namespace _8kyu
                 for (int j = 0; j < a[i].Length; j++)
                 {
                     a[i][j] += b[i][j];
-                }
-            }
-            return a;
-        }
-        public static int[] ArrayDiff(int[] a, int[] b) // TODO do not work, but why?
-        {
-            for (int second = 0; second < b.Length; second++)
-            {
-                for (int first = 0; first < a.Length; first++)
-                {
-                    if (a[first] == b[second])
-                    {
-                        for (int i = first; i < a.Length - 1; i++)
-                        {
-                            a[i] = a[i + 1];
-                        }
-                        Array.Resize(ref a, a.Length - 1);
-                        first = 0;
-                    }
                 }
             }
             return a;
